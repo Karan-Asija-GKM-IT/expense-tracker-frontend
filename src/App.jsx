167f3e2ee110a0b3caf15c2eb3261735e6e91b5f
Karan Asija <karanasija@gkmit.co>
2025-11-14 13:25:04 +0530

    feat: initialize project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>Personal Expense Tracker System</div>
+    </>
+  )
+}
+
+export default App
