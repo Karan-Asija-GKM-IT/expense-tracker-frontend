34757f9bca5786fe2b6f656ea165a3e28b9cd21e
Karan Asija <karanasija@gkmit.co>
2025-11-20 11:07:42 +0530

    feat: add categories constants

---
+++ src/constants/categories.js
@@ -0,0 +1,18 @@
+export const CATEGORIES = [
+  'Bar',
+  'Entertainment',
+  'Eating out',
+  'Shopping, Food - Pets',
+  'Fuel',
+  'Shoes, Clothing',
+  'Technology',
+  'Odd jobs',
+  'Salary',
+  'Rent',
+  'Gifts',
+  'Utilities',
+  'Healthcare',
+  'Transportation',
+  'Education',
+  'Other'
+];
